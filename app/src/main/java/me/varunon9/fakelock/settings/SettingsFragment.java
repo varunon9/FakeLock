76b7bcc8d30182d25dc04f8e9df50c21bfb94538
varunon9 <varunon9@gmail.com>
2017-12-07 00:01:44 +0530

    set background Image: Image picker not working

--- app/src/main/java/me/varunon9/fakelock/settings/SettingsFragment.java
+++ app/src/main/java/me/varunon9/fakelock/settings/SettingsFragment.java
@@ -1,11 +1,19 @@
 package me.varunon9.fakelock.settings;
 
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.provider.MediaStore;
+import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
 import android.support.annotation.Nullable;
 
 import me.varunon9.fakelock.R;
+
+import static android.app.Activity.RESULT_OK;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
 
@@ -19,6 +27,43 @@ public class SettingsFragment extends PreferenceFragmentCompat {
 
         // Load the Preferences from the XML file
         addPreferencesFromResource(R.xml.settings_preferences);
+
+        Preference backgroundPreference = (Preference) findPreference("background_preference");
+        backgroundPreference.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                int PICK_IMAGE = 1;
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE);
+                return true;
+            }
+        });
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+
+        if (resultCode == RESULT_OK) {
+            Uri selectedImage = imageReturnedIntent.getData();
+            System.out.println(selectedImage);
+            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+
+            Cursor cursor = getContext().getContentResolver()
+                    .query(selectedImage, null, null, null, null);
+
+            String filePath;
+            if (cursor == null) {
+                filePath = selectedImage.getPath();
+            } else {
+                cursor.moveToFirst();
+                int columnIndex = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+                filePath = cursor.getString(columnIndex);
+                cursor.close();
+            }
+            System.out.println(filePath);
+        }
     }
 
 }
